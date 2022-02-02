6e123281abe07e4a17ad8c117bdb458475c1eb40
ck-ksst <0822koki@gmail.com>
2022-02-03 02:12:08 +0900

    master

---
+++ src/main/MBBTraining.py
@@ -0,0 +1,92 @@
+# coding: utf-8
+import os
+import random
+import sys
+from timeit import default_timer as timer
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+SCRIPT_NAME = os.path.splitext(__file__)[0]
+SCRIPT_ABS_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.abspath(os.path.join(SCRIPT_ABS_DIR, "..")))
+sys.path.append('..')
+sys.path.append('../lib')
+
+from lib.argument import get_MBBT_args
+from lib.load_data import load_data
+from pbc.model.ClassifierModel import MultiClassKmeansPrototypeClassifier
+from pbc.model.OptimizerModel import GDOptimizer
+from pbc.model.TrainingModel import MaximumBayesBoundarynessTraining
+from pbc.trainer.MBBTrainer import MBBTrainer
+
+
+def main():
+    # set random seed
+    seed = 0
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.set_random_seed(seed)
+
+    # Get arguments
+    config = get_MBBT_args()
+
+    if not os.path.isdir(config.output):
+        os.makedirs(config.output)
+
+    if config.name == "":
+        config.name = "%s-%s" % (os.path.basename(SCRIPT_NAME), os.path.basename(config.dataset).split(".")[0])
+
+    x_train, y_train, x_test, y_test = load_data(config.dataset,
+                                                 delimiter=",",
+                                                 training=config.training_size,
+                                                 testing=config.testing_size,
+                                                 save_split_data=True,
+                                                 load_split_data=True,
+                                                 normalize_by_z_score=True,
+                                                 make_one_hot_label=True,
+                                                 randomize=True)
+
+    cy_train, N_cy_train = np.unique(np.argmax(y_train, axis=1), return_counts=True)
+    minimum_training_sample = np.amin(N_cy_train)
+   
+    if config.proto > minimum_training_sample:
+        print ("WARNING: Too large prototypes (config.proto > min(N_cy))")
+        print ("Overrite config.proto as %s" % minimum_training_sample)
+        config.proto = minimum_training_sample
+
+    # Making Classifier Model
+    classifier_model = MultiClassKmeansPrototypeClassifier(config, x_train, y_train)
+
+    # Making Trainer Model
+    trainer_model = MaximumBayesBoundarynessTraining(classifier_model)
+
+    # Making Optimizer Model
+    optimizer_model = GDOptimizer(classifier_model, trainer_model)
+
+    # Start session
+    sess = tf.Session(graph=tf.get_default_graph())
+
+    # Training model
+    trainer = MBBTrainer(classifier_model,
+                         trainer_model,
+                         optimizer_model,
+                         sess,
+                         config,
+                         x_train,
+                         y_train,
+                         x_test,
+                         y_test)
+
+    # Training
+    trainer.train()
+
+if __name__ == "__main__":
+    print("START: %s" % SCRIPT_NAME)
+    start = timer()
+    main()
+    end = timer()
+    print("DONE: %s" % SCRIPT_NAME)
